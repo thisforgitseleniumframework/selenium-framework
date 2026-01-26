9fb33eedd29a9d8e49f428d75dc4497cd81bd83c
Praveen <thisforgit@rediffmail.com>
2026-01-26 22:43:51 +0530

    Changes to directory

---
+++ src/test/java/com/framework/base/LoginTest.java
@@ -0,0 +1,14 @@
+package com.framework.tests;
+
+import com.framework.base.BaseTest;
+import com.framework.pages.LoginPage;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void validLoginTest() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("Admin", "admin123");
+    }
+}
